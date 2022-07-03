f1ff4780fb8fa4d9e25104275a952ef3ebb88055
TCOALTUNORGU <ogulcan.altunorgu@kktcell.com>
2022-07-03 21:45:15 +0300

    angular-card-app added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  posts = [
+    { imageUrl: 'assets/biking.jpeg', title: 'Dağ Bisikleti', username: '@cbdag', content: 'Çok hızlı sürdüm.' },
+    { imageUrl: 'assets/mountain.jpeg', title: 'Tırmanış', username: '@cbuludag', content: 'Bugün de iyi turmandım.' },
+    { imageUrl: 'assets/tree.jpeg', title: 'Doğa Yürüyüşü', username: '@cbdoga', content: 'Hadi biraz Maçka Parkında turlayalım.' },
+  ];
+
   title = 'card-app';
 }
